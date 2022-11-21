4c1cbf7763f9185cb84c03fbbd4ed42082e85fcf
Shehzad <shehzad.developer92@gmail.com>
2022-11-21 10:08:36 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import ExpFunc from './data-component/Index'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <>
+      <ExpFunc />
+    </>
+
+  );
+}
+
+export default App;
